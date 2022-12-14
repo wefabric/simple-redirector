b27ef199b813e145d501621fe84907b890396f8f
Leo Flapper <dominicus1991@gmail.com>
2022-12-14 12:42:12 +0100

    Fix for uri redirect

--- public/index.php
+++ public/index.php
@@ -15,12 +15,11 @@ if(isset($_SERVER['HTTP_HOST'])) {
     $uri = str_replace(['-', '/'], '_', strtoupper(strtok($_SERVER["REQUEST_URI"], '?')));
 
     $redirect = $_ENV['DEFAULT_REDIRECT'].$_SERVER["REQUEST_URI"];
-
-    if(isset($_ENV[$host])) {
-        $redirect = $_ENV[$host];
-    }
-
-    if($uri !== '_') {
+    if($uri === '_') {
+        if(isset($_ENV[$host])) {
+            $redirect = $_ENV[$host];
+        }
+    } else {
         if(isset($_ENV[$host.$uri])) {
             $redirect = $_ENV[$host.$uri];
         }
